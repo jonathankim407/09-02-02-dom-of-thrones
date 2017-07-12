c6e0040f652ca4aca60ec276d20ef497e2cc480d
jonathankim407 <jonathankim407@gmail.com>
2017-07-12 16:10:32 -0400

    scene 3: dragon's egg
    setup cast
    danerys

--- js/scene3.js
+++ js/scene3.js
@@ -10,7 +10,25 @@ function scene3() {
   // Setup Cast
 
   // Character 1
+  danerysContainer = document.createElement('div')
+  danerysContainer.id = 'danerys-container'
+  stage.append(danerysContainer)
+  danerysContainer.style.width = '10%'
+  danerysContainer.style.position = 'absolute'
+  danerysContainer.style.height = '34%'
+  danerysContainer.style.top = '55%'
+  danerysContainer.style.left = '15%'
 
+  danerys = document.querySelector('#danerys')
+  danerysContainer.append(danerys)
+  danerys.style.width = '48%'
+  danerys.style.position = 'absolute'
+  danerys.style.left = '10%'
+
+  blueDress = document.querySelector('#blue-dress')
+  danerysContainer.append(blueDress)
+  blueDress.style.position = 'absolute'
+  blueDress.style.bottom = 0
   // Character 2
 
   // Setup Props
